653e977e3c53494bfd8ed3f1214cf7d019094276
Alana Gilston <alana@abluescarab.us>
2016-12-26 20:21:32 -0800

    Removed reference to GlobalDyeItem

--- WingSlot.cs
+++ WingSlot.cs
@@ -12,7 +12,6 @@ namespace WingSlot {
             };
 
             AddGlobalItem("GlobalWingItem", new GlobalWingItem());
-            //AddGlobalItem("GlobalDyeItem", new GlobalDyeItem());
         }
     }
 }
