38f6d8cf862fac1c1a60ab6a5a1dfb97d7a1977d
JavidPack <javidpack@gmail.com>
2018-06-22 21:50:17 -0600

    .editorconfig file

--- UIDyeItemSlot.cs
+++ UIDyeItemSlot.cs
@@ -4,75 +4,58 @@ using Terraria.GameContent.Achievements;
 using TerraUI;
 using TerraUI.Objects;
 
-namespace WingSlot
-{
-	// Because I can't change TerraUI, I subclass UIItemSlot to get proper dye slot behavior.
-	internal class UIDyeItemSlot : UIItemSlot
-	{
-		public UIDyeItemSlot(Vector2 position, int size = 52, int context = 0, string hoverText = "", UIObject parent = null, ConditionHandler conditions = null, DrawHandler drawBackground = null, DrawHandler drawItem = null, DrawHandler postDrawItem = null, bool drawAsNormalSlot = false, bool scaleToInventory = false) : base(position, size, context, hoverText, parent, conditions, drawBackground, drawItem, postDrawItem, drawAsNormalSlot, scaleToInventory)
-		{
-		}
+namespace WingSlot {
+    // Because I can't change TerraUI, I subclass UIItemSlot to get proper dye slot behavior.
+    internal class UIDyeItemSlot : UIItemSlot {
+        public UIDyeItemSlot(Vector2 position, int size = 52, int context = 0, string hoverText = "", UIObject parent = null, ConditionHandler conditions = null, DrawHandler drawBackground = null, DrawHandler drawItem = null, DrawHandler postDrawItem = null, bool drawAsNormalSlot = false, bool scaleToInventory = false) : base(position, size, context, hoverText, parent, conditions, drawBackground, drawItem, postDrawItem, drawAsNormalSlot, scaleToInventory) {
+        }
 
-		public override void OnLeftClick()
-		{
-			if (Main.mouseItem.stack == 1 && Main.mouseItem.dye > 0 && Item.type > 0 && Item.type != Main.mouseItem.type)
-			{
-				Utils.Swap<Item>(ref item, ref Main.mouseItem);
-				Main.PlaySound(7, -1, -1, 1, 1f, 0f);
-				if (Item.stack > 0)
-				{
-					AchievementsHelper.HandleOnEquip(Main.LocalPlayer, Item, 12);
-				}
-			}
-			else if (Main.mouseItem.type == 0 && Item.type > 0)
-			{
-				Utils.Swap<Item>(ref item, ref Main.mouseItem);
-				if (Item.type == 0 || Item.stack < 1)
-				{
-					Item = new Item();
-				}
-				if (Main.mouseItem.type == 0 || Main.mouseItem.stack < 1)
-				{
-					Main.mouseItem = new Item();
-				}
-				if (Main.mouseItem.type > 0 || Item.type > 0)
-				{
-					Recipe.FindRecipes();
-					Main.PlaySound(7, -1, -1, 1, 1f, 0f);
-				}
-			}
-			else if (Main.mouseItem.dye > 0 && Item.type == 0)
-			{
-				if (Main.mouseItem.stack == 1)
-				{
-					Utils.Swap<Item>(ref item, ref Main.mouseItem);
-					if (Item.type == 0 || Item.stack < 1)
-					{
-						Item = new Item();
-					}
-					if (Main.mouseItem.type == 0 || Main.mouseItem.stack < 1)
-					{
-						Main.mouseItem = new Item();
-					}
-					if (Main.mouseItem.type > 0 || Item.type > 0)
-					{
-						Recipe.FindRecipes();
-						Main.PlaySound(7, -1, -1, 1, 1f, 0f);
-					}
-				}
-				else
-				{
-					Main.mouseItem.stack--;
-					Item.SetDefaults(Main.mouseItem.type, false);
-					Recipe.FindRecipes();
-					Main.PlaySound(7, -1, -1, 1, 1f, 0f);
-				}
-				if (Item.stack > 0)
-				{
-					AchievementsHelper.HandleOnEquip(Main.LocalPlayer, Item, 12);
-				}
-			}
-			Item.favorited = false;
-		}
-	}
+        public override void OnLeftClick() {
+            if(Main.mouseItem.stack == 1 && Main.mouseItem.dye > 0 && Item.type > 0 && Item.type != Main.mouseItem.type) {
+                Utils.Swap<Item>(ref item, ref Main.mouseItem);
+                Main.PlaySound(7, -1, -1, 1, 1f, 0f);
+                if(Item.stack > 0) {
+                    AchievementsHelper.HandleOnEquip(Main.LocalPlayer, Item, 12);
+                }
+            }
+            else if(Main.mouseItem.type == 0 && Item.type > 0) {
+                Utils.Swap<Item>(ref item, ref Main.mouseItem);
+                if(Item.type == 0 || Item.stack < 1) {
+                    Item = new Item();
+                }
+                if(Main.mouseItem.type == 0 || Main.mouseItem.stack < 1) {
+                    Main.mouseItem = new Item();
+                }
+                if(Main.mouseItem.type > 0 || Item.type > 0) {
+                    Recipe.FindRecipes();
+                    Main.PlaySound(7, -1, -1, 1, 1f, 0f);
+                }
+            }
+            else if(Main.mouseItem.dye > 0 && Item.type == 0) {
+                if(Main.mouseItem.stack == 1) {
+                    Utils.Swap<Item>(ref item, ref Main.mouseItem);
+                    if(Item.type == 0 || Item.stack < 1) {
+                        Item = new Item();
+                    }
+                    if(Main.mouseItem.type == 0 || Main.mouseItem.stack < 1) {
+                        Main.mouseItem = new Item();
+                    }
+                    if(Main.mouseItem.type > 0 || Item.type > 0) {
+                        Recipe.FindRecipes();
+                        Main.PlaySound(7, -1, -1, 1, 1f, 0f);
+                    }
+                }
+                else {
+                    Main.mouseItem.stack--;
+                    Item.SetDefaults(Main.mouseItem.type, false);
+                    Recipe.FindRecipes();
+                    Main.PlaySound(7, -1, -1, 1, 1f, 0f);
+                }
+                if(Item.stack > 0) {
+                    AchievementsHelper.HandleOnEquip(Main.LocalPlayer, Item, 12);
+                }
+            }
+            Item.favorited = false;
+        }
+    }
 }
